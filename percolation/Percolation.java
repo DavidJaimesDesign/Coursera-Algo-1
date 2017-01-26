dab6ac08c6144cb6428e261425834d798c4900f1
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-26 08:28:39 -0800

    Basic grid setup now adding weighted QUF

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,10 +1,47 @@
-public class Percolation {
-   public Percolation(int n)                // create n-by-n grid, with all sites blocked
-   public    void open(int row, int col)    // open site (row, col) if it is not open already
-   public boolean isOpen(int row, int col)  // is site (row, col) open?
-   public boolean isFull(int row, int col)  // is site (row, col) full?
-   public     int numberOfOpenSites()       // number of open sites
-   public boolean percolates()              // does the system percolate?
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-   public static void main(String[] args)   // test client (optional)
+public class Percolation {
+    private array grid;
+    public Percolation(int n)             // create n-by-n grid, with all sites blocked
+   {
+       int size = n * n;
+       Boolean[] genGrid = new boolean[size];
+       for(int i = 0;i < size; i++);
+           genGrid[i] = false;
+       grid = genGrid;
+   }
+   
+   private static double formatCoords(int row, int col)//changed 2d coordinated to 1d coordinates
+   {
+       int formatedCoords;
+       int formatedRow = row - 1;
+       int formatedColumn = col - 1;
+       int rowLength = Math.sqrt(grid.length);
+       formatedCoords = (formatedRow * rowLength) + formatedCol;
+   }
+   public    void open(int row, int col)    // open site (row, col) if it is not open already
+   {
+       int coord = formatCoords(row, col);
+       grid[cord] = true;
+   }
+   public boolean isOpen(int row, int col) // is site (row, col) open?
+   {
+       int coord = formatedCoords(row, col);
+       return grid[coord];
+   }
+   public boolean isFull(int row, int col) // is site (row, col) full?
+   {
+       int coord = formatedCoords(row, col);
+       if(grid[coord]){
+           return false;
+       } else {
+           return true;
+       }
+   }
+   public     int numberOfOpenSites()      // number of open sites
+   public boolean percolates()             // does the system percolate?
+
+   public static void main(String[] args);   // test client (optional)
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
    public double mean()                          // sample mean of percolation threshold
@@ -5,5 +9,5 @@ public class PercolationStats {
    public double confidenceLo()                  // low  endpoint of 95% confidence interval
    public double confidenceHi()                  // high endpoint of 95% confidence interval
 
-   public static void main(String[] args)        // test client (described below)
+   public static void main(String[] args)       // test client (described below)
 }
