8b656257b1654fe25c70077901b00b6139c4e9ad
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-31 08:29:22 -0800

    Writting tests for deque I need to read and fully understand all of this

---
+++ stacks/Assignment/queues/dequeTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;//import test class
+import static org.junit.Assert.*;//import assert methods
+import java.util.Iterator;//import iterator
+
+public class DequeTest {
+    @Test
+    public void testDeque(){
+        Deque<Double> d = new Deque<Double>();//initialize a new deque isntance
+        assertTrue(d.isEmpty());//assert that d is empty
+        d.addFirst(2.0);//add to the first in d
+        assertFalse(d.isEmpty());//assert that d is not empty
+        assertEquals(1, d.size());//test if size works 
+    }
+
+    @Test
+    public void testAddFirstRemoveAndRepeat(){
+        Deque<Double> d = new Deque<Double>();// new deque
+        d.addFirst(1.0);
+        assertEquals(Double.valueOf(1.0),d.removeFirst());
+        d.addLast(2.0);
+        assertEquals(Double.valueOf(2.0),d.removeLast());
+
+        d.addFirst(3.0);
+        assertEquals(Double.valueOf(3.0),d.removeLast());
+        d.addLast(4.0);
+        assertEquals(Double.valueOf(4.0),d.removeFirst());
+    }
+
+    @Test
+    public void testIterator(){
+        Deque<Double> d = new Deque<Double>();
+        for(Double i = 0d; i < 10; i++){
+            d.addLast(i);
+        }
+
+        int i = 0; 
+        for(Double cur: d){
+            System.out.println(i);
+            assertEquals(Double.valueOf(i), Double.valueOf(cur));
+            i++
+        }
+        assertEquals(Double.valueOf(i), Double.valueOf(10));
+    }
+}
+
